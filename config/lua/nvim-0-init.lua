c355a3180af2764b4d0335daa285f5acc883d5be
kit <kitherman@gmail.com>
2024-01-24 21:09:31 -0500

    color section addition

--- config/lua/nvim-0-init.lua
+++ config/lua/nvim-0-init.lua
@@ -5,8 +5,6 @@ vim.o.shiftwidth=2
 vim.opt.ignorecase = true
 vim.o.expandtab = true
 vim.o.smartindent=true
-vim.o.number=true
-vim.cmd('colorscheme 256_noir')
 vim.o.wrap=true
 vim.o.swapfile=false
 vim.o.history=500
