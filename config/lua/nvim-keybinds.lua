58fac7043da425618081b524a3124b3bc9da1373
kit <kitherman@gmail.com>
2024-01-29 08:39:16 -0500

    keybinds update

--- config/lua/nvim-keybinds.lua
+++ config/lua/nvim-keybinds.lua
@@ -8,3 +8,14 @@ vim.api.nvim_set_keymap('x', '<leader>pp', '"+p', opt)
 vim.api.nvim_set_keymap('v', '<leader>pp', '"+p', opt)
 vim.api.nvim_set_keymap('n', '<leader>pp', '"+p', opt)
 
+require("toggleterm").setup{}
+ 
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({cmd = "lazygit", hidden = true, direction = "float"})
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+
